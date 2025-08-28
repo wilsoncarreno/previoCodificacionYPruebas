f76e09c39e79305f7fb9f88a6b6e0c9b8057b8b4
wilsoncarreno <136414588+wilsoncarreno@users.noreply.github.com>
2025-08-28 16:12:00 -0500

    Actualice el archivo admin.py para mejorar la gestión del modelo StockItem, en el panel de administración de Django aplicando principios SOLID y patrones de diseño; creando la clase StockItemAdmin que hereda de admin.ModelAdmin para personalizar la visualización de los registros de StockItem en el panel administrativo, y agregue la propiedad list_display con los campos: id, nombre, cantidad y precio, lo que nos permitio ver directamente en la lista información clave de cada ítem en lugar de mostrar únicamente StockItem object, también agregamos la propiedad search_fields con el campo nombre, habilitando un cuadro de búsqueda en la parte superior de la de lista para filtrar rápidamente los productos por el nombre, y remplaze el registro simple admin.site.register(StockItem) por la versión extendida admin.site.register(StockItem, StockItemAdmin)al realizar esto trate de aplicar el principio srp  ya que el modelo StockItem mantiene la lógica de datos, mientras que la clase StockItemAdmin se encarga de la presentación en el admin, cumpliendo así con que cada clase cumple un único rol, también trae de aplicar e patron de diseño registry Pattern, ya que el modelo queda registrado en un registro centralizado del admin.

--- stock/admin.py
+++ stock/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import StockItem
 
-admin.site.register(StockItem)
+class StockItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre", "cantidad", "precio")
+    search_fields = ("nombre",)
+admin.site.register(StockItem, StockItemAdmin)
 
