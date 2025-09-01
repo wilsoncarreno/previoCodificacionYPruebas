8606604d0a5b0ab900739bed9c1d66829c1ae21e
wilsoncarreno <136414588+wilsoncarreno@users.noreply.github.com>
2025-08-31 20:55:03 -0500

    elimime el codigo que se estaba repitiendo  en create_user y create_superuser porque se estaba repitiendo la logica, use el principio Single Responsibility Principle porque trate de mejorarlo haciendo que Cada método tenga una unica responsabilidad; _create_user este crear el objeto usuario. create_user, este método crear un usuario normal. y create_superuser crea un usuario administrador.

--- stock/models.py
+++ stock/models.py
@@ -2,21 +2,36 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class AdminUserManager(BaseUserManager):
-    def create_user(self, username, password=None):
+    
+    def _create_user(self, username: str, password: str = None, **extra_fields):
+        
         if not username:
             raise ValueError('El usuario debe tener un nombre de usuario')
-        user = self.model(username=username)
+        
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
-
-    def create_superuser(self, username, password):
-        user = self.create_user(username, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.is_active = True
-        user.save(using=self._db)
-        return user
+    
+    def create_user(self, username: str, password: str = None, **extra_fields):
+        
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_active', True)
+        return self._create_user(username, password, **extra_fields)
+    
+    def create_superuser(self, username: str, password: str, **extra_fields):
+        
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser debe tener is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser debe tener is_superuser=True.')
+        
+        return self._create_user(username, password, **extra_fields)
 
 class Administrador(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50, unique=True)
